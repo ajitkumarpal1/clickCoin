1aa525a245f94c8e77e42ae338900517367f16e5
ajitkumarpal1 <66736656+ajitkumarpal1@users.noreply.github.com>
2024-10-23 11:11:06 +0530

    unused velue removed from apollo

--- src/apollo/client.ts
+++ src/apollo/client.ts
@@ -1,5 +1,5 @@
 // src/apollo/client.ts
-import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client';
+import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
   uri: "https://clickcoinbe.onrender.com/graphql",//'http://localhost:2999/graphql',  // Define the GraphQL server URL in .env
